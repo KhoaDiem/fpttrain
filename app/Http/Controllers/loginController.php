b2ec16656be03f66a74afef32864d209b9a21961
github_KhoaDiem <diemtrong01@gmail.com>
2021-12-02 13:47:13 +0700

    fpttrain final

---
+++ app/Http/Controllers/loginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class loginController extends Controller
+{
+    public $users;
+    public function __construct(User $users)
+    {
+        $this->users = $users;
+    }
+    public function login_form(){
+        if(Auth::check()){
+            return redirect()->route('home.home');
+        }else{
+            return view('login.form_login');
+        }
+    }
+    
+    public function login_check(Request $request){
+        if(Auth::check()){
+            return redirect()->route('home.home');
+        }else{
+            if(Auth::attempt(['username' => $request->username , 'password' => $request->pass])){
+                return redirect()->route('home.home');
+            }else{
+                return redirect()->route('login.form')->with('message','username or password not yes');
+            }
+        }
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('home.home');
+    }
+}
